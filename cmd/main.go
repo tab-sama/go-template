717cf60fd880b4092093a83fad063923f16e35cd
Xurvan <xurvan@tutanota.com>
2025-08-02 23:00:13 +0400

    Add some TODO comments

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
+// TODO: This is a placeholder main.go file. Replace with your own application code.
 package main
 
 import "fmt"
 
 func main() {
+	// Replace this with your application's entry point code
 	fmt.Println("Hello, World!!")
 }
